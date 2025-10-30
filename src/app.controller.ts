f3d0b396aed243bb6e05f771dccb9638deabd721
ToEzBit <jirayut52328172@hotmail.com>
2025-10-30 14:16:30 +0700

    :construction: rabbit-mq-consumer

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
+import { EventPattern, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @EventPattern('email')
+  handleEmail(@Payload() data): string {
+    console.log(`Received a new message ${data}`);
+
+    return this.appService.getHello();
+  }
+
+  @EventPattern('kuy')
+  handleEmailKuy(@Payload() data): string {
+    console.log(`kuy Received a new message ${data}`);
+    console.log(data);
+
     return this.appService.getHello();
   }
 }
